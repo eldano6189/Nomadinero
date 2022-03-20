a3f81a10cc6ca990590ef9e3b9daa3d632516cb1
eldano6189 <eldano6189@gmail.com>
2022-03-20 20:28:32 +0000

    test

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,52 +1,10 @@
 import { Button } from "../Buttons/Buttons";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import DataContext from "../../Context/DataContext";
 import styles from "./Form.module.css";
 
 export const Form = () => {
   const { menuHandle } = useContext(DataContext);
-  const [submitText, setSubmitText] = useState(null);
-
-  const onSubmit = async (event, setSubmitText) => {
-    event.preventDefault();
-    setSubmitText("Submitting ...");
-    const formElements = [...event.currentTarget.elements];
-    const isValid =
-      formElements.filter((elem) => elem.name === "bot-field")[0].value === "";
-
-    const validFormElements = isValid ? formElements : [];
-
-    if (validFormElements.length < 1) {
-      // or some other cheeky error message
-      setSubmitText("It looks like you filled out too many fields!");
-    } else {
-      const filledOutElements = validFormElements
-        .filter((elem) => !!elem.value)
-        .map(
-          (element) =>
-            encodeURIComponent(element.name) +
-            "=" +
-            encodeURIComponent(element.value)
-        )
-        .join("&");
-
-      await fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: filledOutElements,
-      })
-        .then(() => {
-          setSubmitText("Successfully submitted!");
-        })
-        .catch((_) => {
-          setSubmitText(
-            "There was an error with your submission, please email me using the address above."
-          );
-        });
-    }
-  };
-
-  console.log(submitText);
 
   return (
     <div className={styles.container}>
@@ -58,7 +16,8 @@ export const Form = () => {
           method="post"
           data-netlify="true"
           netlify-honeypot="bot-field"
-          onSubmit={(e) => onSubmit(e, setSubmitText)}
+          action="/success/"
+          onSubmit="submit"
         >
           <input type="hidden" name="form-name" value="contact" />
           <input type="text" name="name" placeholder="NAME*" required />
