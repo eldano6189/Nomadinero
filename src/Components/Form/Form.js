936c46f3e9b36042416b0806a36a4615f133e48a
eldano6189 <eldano6189@gmail.com>
2022-03-22 20:24:33 +0000

    form redirects

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Button } from "../Buttons/Buttons";
 import { useContext } from "react";
 import DataContext from "../../Context/DataContext";
@@ -5,18 +6,26 @@ import styles from "./Form.module.css";
 
 export const Form = () => {
   const { menuHandle } = useContext(DataContext);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (window.location.search.includes("success=true")) {
+      setSuccess(true);
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.containerForm}>
         <h1>Licence Application Form</h1>
+        {success ? <p>YOUR FORM SUBMISSION HAS BEEN RECIEVED</p> : null}
         <Button onClick={menuHandle} />
         <form
           name="contact"
           method="post"
           data-netlify="true"
           onSubmit="submit"
-          action="/success"
+          action="/success=true"
           data-netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
